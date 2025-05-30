cd3010ce67eef8217d89b619e4e08f52abc6352b
MoonYouBin0 <mubw0080@gmail.com>
2025-05-30 09:52:44 +0900

    소모품 교체일 추가 및 명세서 업데이트

--- gildongE/src/main/java/com/gildong/gildongE/model/Consumable.java
+++ gildongE/src/main/java/com/gildong/gildongE/model/Consumable.java
@@ -3,6 +3,7 @@ package com.gildong.gildongE.model;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDateTime;
 
 @Data
@@ -13,12 +14,23 @@ public class Consumable {
     private String userId;
     private String carModel;
     private String carNumber;
+
+    // 교체 예정일
     private String engineOilDate;
     private String batteryDate;
     private String coolantDate;
     private String transmissionOilDate;
     private String brakeOilDate;
     private String airconFilterDate;
+
+    // 교체한 날짜
+    private String engineOilChangedDate;
+    private String batteryChangedDate;
+    private String coolantChangedDate;
+    private String transmissionOilChangedDate;
+    private String brakeOilChangedDate;
+    private String airconFilterChangedDate;
+
     private LocalDateTime createdAt = LocalDateTime.now();
     private LocalDateTime updatedAt = LocalDateTime.now();
 }
